43bf46344fbb144180d54a55184ec3d62b15f232
ahasparian <a.hasparian@gmail.com>
2024-04-25 19:47:15 +0300

    feat: Add SignupRequest

---
+++ src/main/java/com/clearsolutions/task/solution/data/SignupRequest.java
@@ -0,0 +1,23 @@
+package com.clearsolutions.task.solution.data;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class SignupRequest {
+    @Email(regexp = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*"
+            + "@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$",
+    message = "Email is not valid")
+    private String email;
+    @NotEmpty()
+    private String firstName;
+    @NotEmpty
+    private String lastName;
+    @Past(message = "Birth date must be in the past")
+    @NotNull(message = "Birth date cannot be null")
+    private LocalDate birthDate;
+    private String address;
+    private String phone;
+}
