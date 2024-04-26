9c24b9eeeb966b75f202da804eebf2228996c87e
ahasparian <a.hasparian@gmail.com>
2024-04-26 13:40:55 +0300

    feat: add order. Remove useless tests

--- src/test/java/com/clearsolutions/task/solution/controller/UserControllerTest.java
+++ src/test/java/com/clearsolutions/task/solution/controller/UserControllerTest.java
@@ -3,7 +3,10 @@ package com.clearsolutions.task.solution.controller;
 import com.clearsolutions.task.solution.util.UserUris;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserControllerTest {
 
     @Autowired
@@ -28,6 +32,7 @@ class UserControllerTest {
 
     @Test
     @SneakyThrows
+    @Order(1)
     void createUser_Success() {
         mockMvc.perform(post(UserUris.ROOT)
                         .contentType(MediaType.APPLICATION_JSON)
@@ -37,6 +42,7 @@ class UserControllerTest {
 
     @Test
     @SneakyThrows
+    @Order(3)
     void createUser_shouldThrowBadRequest() {
         mockMvc.perform(post(UserUris.ROOT)
                         .contentType(MediaType.APPLICATION_JSON)
@@ -46,12 +52,8 @@ class UserControllerTest {
 
     @Test
     @SneakyThrows
+    @Order(2)
     void createUser_shouldThrowConflict() {
-        mockMvc.perform(post(UserUris.ROOT)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(getUserRequest(2000))))
-                .andExpect(status().isOk());
-
         mockMvc.perform(post(UserUris.ROOT)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(getUserRequest(2000))))
@@ -60,12 +62,8 @@ class UserControllerTest {
 
     @Test
     @SneakyThrows
+    @Order(4)
     void updateUser_Success() {
-        mockMvc.perform(post(UserUris.ROOT)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(getUserRequest(2000))))
-                .andExpect(status().isOk());
-
         mockMvc.perform(put(UserUris.ROOT)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(getChangedUserRequest(2004))))
@@ -74,6 +72,7 @@ class UserControllerTest {
 
     @Test
     @SneakyThrows
+    @Order(6)
     void updateUser_ShouldThrowNotFound() {
         mockMvc.perform(put(UserUris.ROOT)
                         .contentType(MediaType.APPLICATION_JSON)
@@ -83,18 +82,15 @@ class UserControllerTest {
 
     @Test
     @SneakyThrows
+    @Order(5)
     void deleteUser_Success() {
-        mockMvc.perform(post(UserUris.ROOT)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(getUserRequest(2000))))
-                .andExpect(status().isOk());
-
         mockMvc.perform(delete("/api/v1/user/{email}", "test@example.com"))
                 .andExpect(status().isOk());
     }
 
     @Test
     @SneakyThrows
+    @Order(7)
     void deleteUser_ShouldThrowNotFound() {
         mockMvc.perform(delete(UserUris.EMAIL, "test@example.com"))
                 .andExpect(status().isNotFound());
@@ -102,6 +98,7 @@ class UserControllerTest {
 
     @Test
     @SneakyThrows
+    @Order(8)
     void searchUsersByBirthDateRange() {
         LocalDate fromDate = LocalDate.of(2000, 1, 1);
         LocalDate toDate = LocalDate.of(2005, 12, 31);
@@ -113,6 +110,7 @@ class UserControllerTest {
 
     @Test
     @SneakyThrows
+    @Order(9)
     void searchUsersByBirthDateRange_ShouldThrowBadRequest() {
         LocalDate toDate = LocalDate.of(2000, 1, 1);
         LocalDate fromDate = LocalDate.of(2005, 12, 31);
